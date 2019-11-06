834175c2543b357e54a082c11deb95d8c7319a15
Martin Smolik <smolik.ma@gmail.com>
2019-11-06 15:01:27 +0100

    added a loader

--- utils.py
+++ utils.py
@@ -1,3 +1,15 @@
+import json
+import string
+
+
 def read_corpus(file_path, source='src', verbose=True):
     # return list of list of words
-    raise NotImplementedError
+    corpus=json.load(open(file_path))
+    review_texts=[]
+    for review in corpus:
+        try:
+            text=review["text"].lower()
+        except KeyError: continue
+        text=text.translate(str.maketrans('', '', string.punctuation))
+        review_texts.append(text.split())
+    return review_texts
