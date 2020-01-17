Fixed masking for BERT

models/sentence_encoders.py
@@ -44,7 +44,7 @@ class RandomLSTM(SentenceEncoder):
         self.optimizer = torch.optim.Adam(self.parameters(), lr=tconfig.lr, weight_decay=tconfig.weight_decay)
 
     def forward(self, embedded_words: torch.Tensor, sent_mask: torch.Tensor) -> torch.Tensor:
-        lengths = (-sent_mask + 1).sum(dim=1, keepdim=False)
+        lengths = sent_mask.sum(dim=1, keepdim=False)
         packed_sequence = nn.utils.rnn.pack_padded_sequence(embedded_words, lengths,
                                                             batch_first=True, enforce_sorted=False)
         self.projection.flatten_parameters()
@@ -52,7 +52,7 @@ class RandomLSTM(SentenceEncoder):
         unpacked_sequence, lengths = nn.utils.rnn.pad_packed_sequence(projected_sequence, batch_first=True,
                                                                       total_length=sent_mask.shape[1])
         sent_mask = sent_mask.unsqueeze(2).expand(*sent_mask.shape, self.sentence_dim)
-        unpacked_sequence = unpacked_sequence - sent_mask * 1e9
+        unpacked_sequence = unpacked_sequence - (1-sent_mask) * 1e9
         return unpacked_sequence.max(dim=1, keepdim=False)[0]
 
     def redraw(self):

utils/helpers.py
@@ -30,7 +30,7 @@ def makedirs(name):
 
 def prepare_sentences(sentences: List[List[str]], vocab):
     lengths = [len(sentence) for sentence in sentences]
-    mask = [[0 for _ in range(length)] + [1 for _ in range(max(lengths) - length)] for length in lengths]
+    mask = [[1 for _ in range(length)] + [0 for _ in range(max(lengths) - length)] for length in lengths]
     mask = torch.FloatTensor(mask)
     q = [sent for sent in sentences]
     np_sents = [np.array([vocab[word] for word in sent]) for sent in sentences]
@@ -47,7 +47,7 @@ def prepare_sentences(sentences: List[List[str]], vocab):
 
 def make_masks(sequences):
     lengths = [len(sentence) for sentence in sequences]
-    mask = [[0 for _ in range(length)] + [1 for _ in range(max(lengths) - length)] for length in lengths]
+    mask = [[1 for _ in range(length)] + [0 for _ in range(max(lengths) - length)] for length in lengths]
     mask = torch.FloatTensor(mask)
 
     if GPU: